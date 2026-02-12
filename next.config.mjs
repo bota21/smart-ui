e1bafba672ecb3ff93108908374eda6c944b78bd
Botagoz Aimkulova <aimkulow@bk.ru>
2026-02-12 10:54:55 +0500

    edit nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  experimental: {
+    appDir: true, // обязательно для App Router
+  },
 };
 
 export default nextConfig;
