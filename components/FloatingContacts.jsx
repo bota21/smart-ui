e71f58aa91f9cbd141b0f71d51b1b3ac4477f5cc
Botagoz Aimkulova <aimkulow@bk.ru>
2026-02-12 15:21:58 +0500

    add meta tag

--- components/FloatingContacts.jsx
+++ components/FloatingContacts.jsx
@@ -13,8 +13,8 @@ export default function FloatingContacts() {
           className="floating-menu"
           style={{ display: isFloatingMenuOpen ? "flex" : "none" }}
         >
-          <a href="tel:+77778410050" class="btn-primary">
-            <svg class="icon call" viewBox="0 0 24 24" aria-hidden="true">
+          <a href="tel:+77778410050" className="btn-primary">
+            <svg className="icon call" viewBox="0 0 24 24" aria-hidden="true">
               <path d="M6.6 10.8c1.2 2.3 3.1 4.2 5.4 5.4l1.8-1.8c.3-.3.7-.4 1.1-.3 1.2.4 2.5.6 3.8.6.6 0 1 .4 1 1v2.9c0 .6-.4 1-1 1C10.3 20.6 3.4 13.7 3.4 5.6c0-.6.4-1 1-1H7.3c.6 0 1 .4 1 1 0 1.3.2 2.6.6 3.8.1.4 0 .8-.3 1.1l-1.9 1.3z" />
             </svg>
             <span data-i18n="floating_call">Позвонить</span>
@@ -23,9 +23,13 @@ export default function FloatingContacts() {
           <a
             href="https://t.me/77778410050"
             target="_blank"
-            class="btn-primary"
+            className="btn-primary"
           >
-            <svg class="icon telegram" viewBox="0 0 24 24" aria-hidden="true">
+            <svg
+              className="icon telegram"
+              viewBox="0 0 24 24"
+              aria-hidden="true"
+            >
               <path d="M9.9 15.6 9.6 19c.4 0 .6-.2.8-.4l1.9-1.8 3.9 2.8c.7.4 1.2.2 1.4-.6l2.6-12.2c.2-.9-.3-1.3-1-1L4.2 9.6c-.8.3-.8.8-.1 1l4.2 1.3L17 6.9c.4-.3.8-.1.5.2z" />
             </svg>
             <span data-i18n="floating_telegram">Telegram</span>
@@ -34,9 +38,13 @@ export default function FloatingContacts() {
           <a
             href="https://wa.me/77778410050"
             target="_blank"
-            class="btn-primary"
+            className="btn-primary"
           >
-            <svg class="icon whatsapp" viewBox="0 0 24 24" aria-hidden="true">
+            <svg
+              className="icon whatsapp"
+              viewBox="0 0 24 24"
+              aria-hidden="true"
+            >
               <path d="M12 2a10 10 0 0 0-8.7 15l-1.1 4.1 4.2-1.1A10 10 0 1 0 12 2zm5.5 14.5c-.2.6-1.2 1.1-1.7 1.1-.5.1-1.1.1-3.6-1.2-3.1-1.6-5-4.4-5.1-4.6-.1-.2-1.2-1.6-1.2-3 0-1.4.7-2.1 1-2.4.3-.3.6-.4.8-.4h.6c.2 0 .5-.1.7.5.2.6.8 2.2.9 2.4.1.2.1.4 0 .6-.1.2-.2.4-.4.6-.2.2-.4.4-.6.6-.2.2-.4.4-.2.8.2.4 1 1.6 2.1 2.6 1.5 1.3 2.8 1.7 3.2 1.9.4.2.6.2.9-.1.3-.3 1-1.2 1.3-1.6.3-.4.6-.3 1-.1.4.2 2.5 1.2 2.9 1.4.4.2.6.3.7.5.1.2.1.6-.1 1.2z" />
             </svg>
             <span data-i18n="floating_whatsapp">WhatsApp</span>
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,21 @@
 import "@/styles/style.css";
 import "@/styles/layout.css";
+import Head from "next/head";
 import { LanguageProvider } from "@/context/LanguageContext";
 import LayoutContent from "@/components/LayoutContent";
 
 export default function App({ Component, pageProps }) {
   return (
     <LanguageProvider>
+      <Head>
+        <title>SmartÜi — Умный дом с Алисой</title>
+        <link
+          rel="icon"
+          // href="/assets/logo/Shanyrak-black.png"
+          type="image/png"
+        />{" "}
+      </Head>
+
       <LayoutContent>
         <Component {...pageProps} />
       </LayoutContent>
